4169ef6a97c689fb998add243061485d6148f91d
Pavel lysenko <p.lysenko@zmeke.com>
2016-10-26 18:13:29 +0300

    1.0.0

---
+++ readme.txt
@@ -0,0 +1,13 @@
+=== WooCommerce field vendor URL ===
+Contributors: ohar
+Donate link: https://www.paypal.me/ohar
+Tags: buy, price, woocommerce, product, custom, field
+License: MIT
+License URI: https://github.com/Ohar/wc-field-buy-price/blob/master/LICENSE
+
+Plugin just adds custom field “buy_price” to WooCommerce products. No more, no less.
+
+== Installation ==
+
+1. Upload the plugin files to the `/wp-content/plugins/wc-field-buy-price` directory, or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the 'Plugins' screen in WordPress
---
+++ wc-field-vendor-url.php
@@ -0,0 +1,57 @@
+<?php
+/*
+Plugin Name: WooCommerce field buy price
+Plugin URI: https://github.com/Ohar/wc-field-buy-price
+Description:  Add custom field “buy_price” to the WooCommerce products
+Author: Pavel Lysenko aka Ohar
+Author URI: http://ohar.name/
+Contributors: ohar
+Version: 1.0.0
+License: MIT
+Text Domain: wc-field-buy-price
+Domain Path: /languages
+*/
+
+// Inspired by http://www.remicorson.com/mastering-woocommerce-products-custom-fields/
+
+// Display Fields
+add_action( 'woocommerce_product_options_general_product_data', 'add_custom_woocommerce_general_field_buy_price' );
+
+// Save Fields
+add_action( 'woocommerce__process_product_meta', 'save_custom_woocommerce_general_field_buy_price' );
+
+function add_custom_woocommerce_general_field_buy_price() {
+
+  global $woocommerce, $post;
+
+  echo '<div class="options_group">';
+
+	woocommerce_wp_text_input(
+		array(
+			'id'                => 'buy_price',
+			'label'             => __( 'Buy Price', 'wc-field-buy-price' ),
+			'placeholder'       => '0',
+			'description'       => __( 'Price of buying product from vendor', 'wc-field-buy-price' ),
+			'type'              => 'number',
+			'custom_attributes' => array(
+				'step' 	=> 'any',
+				'min'	  => '0'
+			)
+)
+	);
+
+	echo '</div>';
+}
+
+function save_custom_woocommerce_general_field_buy_price( $post_id ) {
+	$buy_price = $_POST['buy_price'];
+	if (!empty( $buy_price ) ) {
+		update_post_meta( $post_id, 'buy_price', esc_attr( $buy_price ) );
+	}
+}
+
+add_action( 'plugins_loaded', 'load_wc_field_buy_price_textdomain' );
+
+function load_wc_field_buy_price_textdomain() {
+	load_plugin_textdomain( 'wc-field-buy-price', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
